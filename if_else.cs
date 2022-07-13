aa2cac55608d3f03440cfa1577f9bf66fcabe030
mimmarry <105546871+mimmarry@users.noreply.github.com>
2022-07-14 00:04:46 +0300

    Add files via upload

---
+++ if_else.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class Programm
+{
+    static void Main(string[] args)
+    {
+        //Напишите программу ,проверяющее число,введенное с клавиатуры на четность 
+        double chislo;
+        Console.WriteLine("Введите число");
+        chislo = double.Parse(Console.ReadLine());
+            if (chislo % 2 == 0)
+        {
+            Console.WriteLine("Число четное");
+        }
+        else 
+        {
+            Console.WriteLine("Число нечетное");
+        }
+    }
+}
